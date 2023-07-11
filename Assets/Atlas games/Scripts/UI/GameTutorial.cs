383ae3503f661982f056b0b34cb4f0e6f004b809
Amir Safari <77434000+amirsafaricg@users.noreply.github.com>
2023-07-11 10:04:21 +0330

    Game tutorial completed
    Game tutorial completed with more vast types of tutorials:
    - tips (legacy)
    - pop up
    - task

--- Assets/Atlas games/Scripts/UI/GameTutorial.cs
+++ Assets/Atlas games/Scripts/UI/GameTutorial.cs
@@ -2,28 +2,75 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
 
 public class GameTutorial : MonoBehaviour
 {
-    public Transform[] uiParts;
+    public enum TipType
+    {
+        Dialog,
+        Tip,
+        Task
+    }
+
+    public enum Direction
+    {
+        Top,
+        Bottom,
+        Left,
+        Right,
+        UpperLeft,
+        UpperRight,
+        BottomLeft,
+        BottomRight
+    }
+
+
+    [HideInInspector] public TipType type;
+    [HideInInspector] public Direction direction;
+
+
+    public List<TipSetup> tipsList;
+    [HideInInspector] public Transform[] uiParts;
     public Transform mask;
-    public GameObject[] tips;
-    public float[] scale;
     public GameObject darkBackground;
-    private int _tipIndex;
+    public GameObject dialogBackground;
+    public Transform pointerObject;
+    public Transform pointerIcon;
     private int _tipOrder;
     public float speed = 30f;
     private float _maxDistance = 0.01f;
 
+
+    [Serializable]
+    public class TipSetup
+    {
+        public string openTrigger;
+        public string closeTrigger;
+        public string pointerDirection;
+        public float delay;
+        public bool isLastDialog;
+        public Animator tipAnimator;
+        public Transform uiPart;
+        public float scale;
+        public string type;
+        public bool isUiInteractible;
+    }
+
+    public string tutorialName;
+
+   public List<Button> buttons = new List<Button>();
+
     // start game and open tutorial automatically if never watched 
     void Start()
     {
-        _tipIndex = uiParts.Length;
         _tipOrder = -1;
-
-        if (GlobalValue.GameTutorialOpened != 1)
+        StartCoroutine(OpenTutorialAtStart());
+        pointerIcon.gameObject.SetActive(false);
+        if (PlayerPrefs.GetInt("GameTutorial" + tutorialName) != 1)
         {
-            StartCoroutine(OpenTutorialAtStart());
+            //play tutorial
         }
     }
 
@@ -32,53 +79,171 @@ public class GameTutorial : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         StartTutorial();
-        GlobalValue.GameTutorialOpened = 1;
+        
+        //makes sure that tutorial doesnt play anymore at the start of the scene 
+        PlayerPrefs.SetInt("GameTutorial" + tutorialName, 1);
     }
 
 // start tutorial with first tip and pause game 
     public void StartTutorial()
     {
         NextTip();
-        Time.timeScale = 0;
-        darkBackground.SetActive(true);
     }
 
 // go to next tip and animate enter/exit states of tips 
     public void NextTip()
     {
         _tipOrder++;
-
-        if (_tipOrder <= tips.Length - 1)
+        // close last one 
+        if (_tipOrder > 0)
         {
-            StartCoroutine(SmoothTransition(uiParts[_tipOrder].transform.position, scale[_tipOrder]));
-            tips[_tipOrder].GetComponent<Animator>().SetTrigger("Open");
+            CloseTip();
+            OpenTip();
         }
         else
         {
-            darkBackground.SetActive(false);
-            Time.timeScale = 1;
-            _tipOrder = -1;
-            tips[tips.Length - 1].GetComponent<Animator>().SetTrigger("Close");
+            OpenTip();
         }
+    }
 
-
+    void CloseTip()
+    {
         if (_tipOrder > 0)
         {
-            tips[_tipOrder - 1].GetComponent<Animator>().SetTrigger("Close");
+            TipSetup prevSetup = tipsList[_tipOrder - 1];
+            switch (prevSetup.type)
+            {
+                case "Dialog":
+                    if (prevSetup.isLastDialog)
+                    {
+                        prevSetup.tipAnimator.SetTrigger(prevSetup.closeTrigger);
+                    }
+
+                    break;
+                case "Tip":
+                    prevSetup.tipAnimator.SetTrigger(prevSetup.closeTrigger);
+                    break;
+                case "Task":
+                    pointerObject.gameObject.SetActive(false);
+                    pointerIcon.gameObject.SetActive(false);
+                    
+                    if (!prevSetup.isUiInteractible)
+                    {
+                        foreach (Button button in buttons)
+                        { 
+                            button.interactable = true;
+                        }
+                        buttons.Clear();
+                    }
+                    prevSetup.uiPart.GetComponent<Button>().onClick.RemoveListener(NextTip);
+                    break;
+            }
         }
     }
 
 
-    // set position and scale of mask based on the part of the tutorial
+    public void OpenTip()
+    {
+        if (_tipOrder <= tipsList.Count - 1)
+        {
+            TipSetup nextSetup = tipsList[_tipOrder];
+            switch (nextSetup.type)
+            {
+                case "Dialog":
+                    nextSetup.tipAnimator.SetTrigger(nextSetup.openTrigger);
+                    darkBackground.SetActive(false);
+                    dialogBackground.SetActive(true);
+                    mask.gameObject.SetActive(false);
+                    dialogBackground.SetActive(true);
+                    Time.timeScale = 0;
+                    break;
+                case "Task":
+                    if (!nextSetup.isUiInteractible)
+                    {
+                        Button[] buttonsActive = FindObjectsOfType<Button>();
+                        foreach (Button button in buttonsActive)
+                        {
+                            if (button.IsInteractable())
+                            {
+                                button.interactable = false;
+                                buttons.Add(button);
+                            }
+                        }   
+                    }
+                    nextSetup.uiPart.GetComponent<Button>().interactable = true;
+                    nextSetup.uiPart.GetComponent<Button>().onClick.AddListener(NextTip);
+                    Thread.Sleep(Mathf.RoundToInt(nextSetup.delay * 1000));
+                    pointerObject.gameObject.SetActive(true);
+                    pointerIcon.gameObject.SetActive(true);
+                    pointerObject.transform.position = nextSetup.uiPart.position;
+                    for (int a = 0; a < pointerObject.childCount; a++)
+                    {
+                        if (pointerObject.GetChild(a).name == nextSetup.pointerDirection)
+                        {
+                            pointerIcon.transform.position = pointerObject.GetChild(a).position;
+                            switch (nextSetup.pointerDirection)
+                            {
+                                case "Top":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, 0, 0);
+                                    break;
+                                case "Bottom":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, 180, 0);
+                                    break;
+                                case "Left":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, -90, 0);
+                                    break;
+                                case "Right":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, 90, 0);
+                                    break;
+                                case "TopLeft":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, -45, 0);
+                                    break;
+                                case "TopRight":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, 45, 0);
+                                    break;
+                                case "BottomLeft":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, -135, 0);
+                                    break;
+                                case "BottomRight":
+                                    pointerIcon.transform.rotation = new Quaternion(0, 0, 135, 0);
+                                    break;
+                            }
+                        }
+                    }
+
+                    darkBackground.SetActive(false);
+                    mask.gameObject.SetActive(false);
+                    dialogBackground.SetActive(false);
+                    Time.timeScale = 1;
+                    break;
+                case "Tip":
+                    nextSetup.tipAnimator.SetTrigger(nextSetup.openTrigger);
+                    Time.timeScale = 0;
+                    darkBackground.SetActive(true);
+                    mask.gameObject.SetActive(true);
+                    dialogBackground.SetActive(false);
+                    StartCoroutine(SmoothTransition(nextSetup.uiPart.position, nextSetup.scale));
+                    break;
+            }
+        }
+        else
+        {
+            darkBackground.SetActive(false);
+            dialogBackground.SetActive(false);
+            Time.timeScale = 1;
+            _tipOrder = -1;
+        }
+    }
+
 
     IEnumerator SmoothTransition(Vector3 targetPosition, float targetScale)
     {
         float appliedScale = mask.localScale.x;
         Vector3 maskPosition = mask.position;
-        while (Vector3.Distance(mask.position,uiParts[_tipOrder].position)>_maxDistance)
+        while (Vector3.Distance(mask.position, targetPosition) > _maxDistance)
         {
-            maskPosition = Vector3.Lerp(mask.position, uiParts[_tipOrder].position, Time.unscaledDeltaTime * speed);
-            appliedScale = Mathf.Lerp(mask.localScale.x, scale[_tipOrder], Time.unscaledDeltaTime * speed);
+            maskPosition = Vector3.Lerp(mask.position, targetPosition, Time.unscaledDeltaTime * speed);
+            appliedScale = Mathf.Lerp(mask.localScale.x, targetScale, Time.unscaledDeltaTime * speed);
             mask.localScale = new Vector3(appliedScale, appliedScale, appliedScale);
             mask.position = maskPosition;
             yield return null;
---
+++ Assets/Editor/TipEditor.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine;
+
+[CustomEditor(typeof(GameTutorial))]
+public class TipEditor : Editor
+{
+    private SerializedProperty _type;
+
+    // if tip type is dialog : 
+    private GameObject _dialogDescription;
+
+    //   private Animator _dialog;
+    private string _openTrigger;
+    private string _closeTrigger;
+    private bool _isLast;
+
+    // if tip type is task : 
+    private float _delay;
+    private Transform _pointerObject;
+    private SerializedProperty _direction;
+    private Button _buttonTransform;
+    
+    // if tip type is just tip : 
+    private Animator _tipAnimator;
+    private Transform _uiPart;
+    private float _scale = 0;
+    private bool _isUiInteractible;
+
+    // properties
+    public List<GameTutorial.TipSetup> setups;
+    private GameTutorial.TipSetup _setup;
+    private GameTutorial _gt;
+
+    private void OnEnable()
+    {
+        _setup = new GameTutorial.TipSetup();
+        setups = new List<GameTutorial.TipSetup>();
+        _gt = (GameTutorial)target;
+        setups = _gt.tipsList;
+        _gt.tipsList = setups;
+        _type = serializedObject.FindProperty("type");
+        _direction = serializedObject.FindProperty("direction");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Space(10);
+
+        EditorGUILayout.LabelField("Update script variables", EditorStyles.boldLabel);
+        GUILayout.Space(10);
+
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(_type);
+
+        if (_gt.type == GameTutorial.TipType.Dialog)
+        {
+            _isLast = EditorGUILayout.Toggle("Is the last tip of dialog", _isLast);
+            if (_isLast)
+            {
+                _closeTrigger = EditorGUILayout.TextField("Animation Close Trigger", _closeTrigger);
+            }
+
+            _openTrigger = EditorGUILayout.TextField("Animation Open Trigger", _openTrigger);
+            _tipAnimator = (Animator)EditorGUILayout.ObjectField("Tip Animation", _tipAnimator, typeof(Animator), true);
+        }
+        else if (_gt.type == GameTutorial.TipType.Task)
+        {
+            _isUiInteractible = EditorGUILayout.Toggle("Can Interact with UI ", _isUiInteractible);
+            EditorGUILayout.PropertyField(_direction);
+            _delay = EditorGUILayout.FloatField("Start Pointing delay", _delay);
+            _buttonTransform = (Button)EditorGUILayout.ObjectField("Target Button", _buttonTransform, typeof(Button), true);
+        }
+        else if (_gt.type == GameTutorial.TipType.Tip)
+        {
+            _tipAnimator = (Animator)EditorGUILayout.ObjectField("Tip Animation", _tipAnimator, typeof(Animator), true);
+            _uiPart = (Transform)EditorGUILayout.ObjectField("UI Element", _uiPart, typeof(Transform), true);
+            _scale = EditorGUILayout.FloatField("Mask Scale ", _scale);
+            _closeTrigger = EditorGUILayout.TextField("Animation Close Trigger", _closeTrigger);
+            _openTrigger = EditorGUILayout.TextField("Animation Open Trigger", _openTrigger);
+        }
+
+        GameTutorial script = (GameTutorial)target;
+        GUILayout.Space(10);
+        GUI.backgroundColor = Color.green;
+        if (GUILayout.Button("Add Tip", GUILayout.Height(30)))
+        {
+            Reset();
+            if (_gt.type == GameTutorial.TipType.Dialog)
+            {
+                if ((_isLast && _openTrigger != null && _closeTrigger != null) || _openTrigger != null)
+                {
+                    _setup.openTrigger = _openTrigger;
+                    _setup.tipAnimator = _tipAnimator;
+                    _setup.closeTrigger = _closeTrigger;
+                    _setup.type = _gt.type.ToString();
+                    _setup.isLastDialog = _isLast;
+                    setups.Add(_setup);
+                }
+            }
+            else if (_gt.type == GameTutorial.TipType.Task)
+            {
+                if (_buttonTransform != null && _direction != null)
+                {
+                    _setup.delay = _delay;
+                    _setup.pointerDirection = _gt.direction.ToString();
+                    _setup.type = _gt.type.ToString();
+                    _setup.isUiInteractible = _isUiInteractible;
+                    _setup.uiPart = _buttonTransform.transform;
+                    setups.Add(_setup);
+                }
+            }
+            else if (_gt.type == GameTutorial.TipType.Tip)
+            {
+                if (_openTrigger != null && _closeTrigger != null && _uiPart && _tipAnimator)
+                {
+                    _setup.tipAnimator = _tipAnimator;
+                    _setup.uiPart = _uiPart;
+                    _setup.scale = _scale;
+                    _setup.openTrigger = _openTrigger;
+                    _setup.closeTrigger = _closeTrigger;
+                    _setup.type = _gt.type.ToString();
+                    setups.Add(_setup);
+                }
+            }
+        }
+
+        GUI.backgroundColor = Color.red;
+        if (GUILayout.Button("Reset", GUILayout.Height(30)))
+        {
+            Reset();
+            setups.Clear();
+        }
+
+        GUI.backgroundColor = Color.white;
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Current tutorial steps", EditorStyles.boldLabel);
+        GUILayout.Space(10);
+        serializedObject.ApplyModifiedProperties();
+        _gt.tipsList = setups;
+        DrawDefaultInspector();
+    }
+
+    private void Reset()
+    {
+        try
+        {
+            _setup.openTrigger = null;
+            _setup.closeTrigger = null;
+            _setup.isLastDialog = false;
+            _setup.pointerDirection = null;
+            _setup.delay = 0.5f;
+            _setup.tipAnimator = null;
+            _setup.uiPart = null;
+            _setup.scale = 0;
+            _setup.isUiInteractible = false;
+            _setup.type = null;
+        }
+        catch (Exception e)
+        {
+        }
+    }
+}
