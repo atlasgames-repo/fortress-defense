f02af908069e25e4bec9dc161655fe6b2bc96610
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-07-24 11:46:31 +0330

    add garbage collection for expired tasks, removeing them after 1 day

--- Assets/Atlas games/Scripts/Achievements V2/AchievementScheduler.cs
+++ Assets/Atlas games/Scripts/Achievements V2/AchievementScheduler.cs
@@ -10,18 +10,19 @@ public class AchievementScheduler : BasePlayerPrefs<AchievementScheduleModel>
 
     public float InitialDelaySeconds;
     public float ListenerTickSeconds;
+    public int HoldExpiredScheduleMinutes;
     public AnimationCurve Probability;
     public AchievementScheduleModel[] Schedules;
     private Coroutine Dispacher;
 
     void Start()
     {
-        
+
         Dispacher = StartCoroutine(Listener());
     }
     IEnumerator Listener()
     {
-        
+
         yield return new WaitForSeconds(InitialDelaySeconds);
         while (true)
         {
@@ -39,11 +40,11 @@ public class AchievementScheduler : BasePlayerPrefs<AchievementScheduleModel>
                     foreach (AchievementModel item in BasePlayerPrefs<AchievementModel>.DictArray.Where(a => a.Schedul_id == foundSchedule._id).ToArray()) // deactivate the expired Tasks
                     {
                         item.isActive = expired < 0 && item.isActive && item.status != TrophyStatus.PAYED;
-                        
+
                         if (item.status == TrophyStatus.PAYED) successfullTasks++;
                         BasePlayerPrefs<AchievementModel>.Update(item._id, item);
                     }
-                    foundSchedule.status = successfullTasks < foundSchedule.NumberOfMissions ? expired >= 0 ? ScheduleStatus.EXPIRED: ScheduleStatus.PENDING : ScheduleStatus.DONE;
+                    foundSchedule.status = successfullTasks < foundSchedule.NumberOfMissions ? expired >= 0 ? ScheduleStatus.EXPIRED : ScheduleStatus.PENDING : ScheduleStatus.DONE;
                     Update(foundSchedule._id, foundSchedule);
                     if (foundSchedule.status == ScheduleStatus.PENDING) continue;
                 }
@@ -51,29 +52,46 @@ public class AchievementScheduler : BasePlayerPrefs<AchievementScheduleModel>
                 AchievementScheduleModel new_schedule = new AchievementScheduleModel(schedules.type, schedules.NumberOfMissions, schedules.name);
 
                 Add(new_schedule._id, new_schedule);
-                
+
                 for (int i = 0; i < schedules.NumberOfMissions; i++)
                 {
                     AchievementModel new_achievement = GetRandomAchievemntByType(GetAchievementType);
                     new_achievement._id = Guid.NewGuid();
                     new_achievement.Schedul_id = new_schedule._id;
-                    AchievementTasksV2.self.AddNewAchievements(new AchievementModel[1]{new_achievement});
-                } 
+                    AchievementTasksV2.self.AddNewAchievements(new AchievementModel[1] { new_achievement });
+                }
+            }
+            // check if the schedule is expired and delete its Task and the Schedule
+            AchievementScheduleModel foundEndedSchedule = DictArray.Where(a => a.status != ScheduleStatus.PENDING).FirstOrDefault();
+            if (foundEndedSchedule != null)
+            {
+                int passed_delettion = DateTime.Compare(DateTime.Now, foundEndedSchedule.ExpireDate.AddMinutes(HoldExpiredScheduleMinutes));
+                if (passed_delettion >= 0)
+                {
+                    foreach (AchievementModel item in BasePlayerPrefs<AchievementModel>.DictArray.Where(a => a.Schedul_id == foundEndedSchedule._id).ToArray())
+                    {
+                        BasePlayerPrefs<AchievementModel>.Remove(item._id);
+                    }
+                    Remove(foundEndedSchedule._id);
+                }
             }
             yield return new WaitForSeconds(ListenerTickSeconds);
         }
     }
-    public AchievementModel GetRandomAchievemntByType(AchievementType _type){
+    public AchievementModel GetRandomAchievemntByType(AchievementType _type)
+    {
         return data.models.Where(achiv => achiv.type == _type).OrderBy(x => UnityEngine.Random.value).First();
     }
-    public AchievementType GetAchievementType{
-        get{
-            float P = UnityEngine.Random.Range(0f,1f);
+    public AchievementType GetAchievementType
+    {
+        get
+        {
+            float P = UnityEngine.Random.Range(0f, 1f);
             float C = Probability.Evaluate(P);
             int i = Mathf.RoundToInt(C);
             return (AchievementType)i;
         }
     }
-    
+
 }
 
