bbc4ff318fb2368b5e5b720e0c974b17feb1ec9d
amirsafaricg <a.safari1380@gmail.com>
2024-03-12 09:39:43 +0330

    changed to async

--- Assets/Atlas games/Scripts/LeaderBoard/LeaderboardListItem.cs
+++ Assets/Atlas games/Scripts/LeaderBoard/LeaderboardListItem.cs
@@ -10,15 +10,10 @@ public class LeaderboardListItem : MonoBehaviour
     public TMP_Text rxpText;
     public Image image;
     // this function sets the data recived from leaderboard.cs to the list item component 
-    public void SetData(LeaderboardData data)
+    public async void SetData(LeaderboardData data)
     {
        rxpText.text = data.rank.ToString();
        userNameText.text = data.user_name;
-       SetImage(data.user_avatar);
-    }
-    
-    async void SetImage(string url)
-    {
-        
+       image.sprite = await APIManager.instance.Get_rofile_picture(data.user_avatar);
     }
 }
