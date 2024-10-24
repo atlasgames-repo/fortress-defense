e00b32fa25f4d2116944e6dc1438f1bfb06665ed
Amir Safari <77434000+amirsafaricg@users.noreply.github.com>
2024-10-24 12:54:29 +0330

    add system linq

--- Assets/_FD/Script/GUI/MenuManager.cs
+++ Assets/_FD/Script/GUI/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using System.Linq;
 public class MenuManager : MonoBehaviour, IListener
 {
     public static MenuManager Instance;
