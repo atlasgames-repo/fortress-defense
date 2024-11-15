5c2d36c25a24e2c378918996d7d60e2facd449f1
Amir Safari <77434000+amirsafaricg@users.noreply.github.com>
2024-11-15 20:15:31 +0330

    Added hint basics

--- Assets/Atlas games/Scripts/Tutorial 2.0/Scripts/Hint.cs
+++ Assets/Atlas games/Scripts/Tutorial 2.0/Scripts/Hint.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Hint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text hintText;
+    public Transform TL_Pos;
+    public Transform TM_Pos;
+    public Transform TR_Pos;
+    public Transform L_Pos;
+    public Transform R_Pos;
+    public Transform BL_Pos;
+    public Transform BR_Pos;
+    public Transform BM_Pos;
+
+    public void Show(string text, Direction direction)
     {
-        
+        hintText.text = text;
+        switch (direction)
+        {
+            case Direction.Left:
+                
+                break;
+            case Direction.Right:
+                
+                break;
+            case Direction.Top:
+                break;
+            case Direction.BottomLeft:
+                break;
+            case Direction.BottomRight: 
+                break;
+            case Direction.UpperLeft:
+                
+                break;
+            case Direction.UpperRight :
+                break;
+            case Direction.Bottom:
+                
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hide()
     {
         
     }
