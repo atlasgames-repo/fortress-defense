ef493cf3be1ca8fbd99bf0509bb4ebfdba0ca48e
Amir Safari <77434000+amirsafaricg@users.noreply.github.com>
2024-12-07 15:35:58 +0330

    Revert "Update Assets/Atlas games/Scripts/Tutorial 2.0/Scripts/GameTutorialManager.cs"
    This reverts commit 07b436a01ad7bd7651d9172bffd725edefeb43ff.

--- Assets/Atlas games/Scripts/Tutorial 2.0/Scripts/GameTutorialManager.cs
+++ Assets/Atlas games/Scripts/Tutorial 2.0/Scripts/GameTutorialManager.cs
@@ -13,13 +13,8 @@ public class GameTutorialManager : MonoBehaviour
                 if (!setupObj) return;
                 if (setup.SceneTutorial() != null)
                 {
-                    MenuManager manager = FindObjectOfType<MenuManager>();
-                    if (manafer == null) {
-                        // do something logical
-                        return;
-                    }
-                    GameObject obj = Instantiate(setup.SceneTutorial(), manager.transform.position, Quaternion.identity, manager.transform);
-                    obj.transform.SetSiblingIndex(manager.transform.childCount - 1);
+                    GameObject obj = Instantiate(setup.SceneTutorial(), FindObjectOfType<MenuManager>().transform.position, Quaternion.identity, FindObjectOfType<MenuManager>().transform);
+                    obj.transform.SetSiblingIndex(FindObjectOfType<MenuManager>().transform.childCount - 1);
                     obj.GetComponent<TutorialNew>().InitTutorial();
                 }
            
